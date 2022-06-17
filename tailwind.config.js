88272bb21e8bc78b064034ba308161fc90be283f
Vu Gia Khanh <khanh26102003a@gmail.com>
2022-06-17 14:01:32 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#E5E5E5",
+        is_active: "#163668",
+        btn_red: "#df142d",
+        btn_blue: "#0a6ada",
+      },
+    },
+  },
+  plugins: [],
+};
